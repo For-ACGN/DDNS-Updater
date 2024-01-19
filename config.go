4efef8cb1de98306cd0724121068b8feb90c5b3c
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-01-19 12:40:38 +0800

    add Configuration for Updater, add patch about type duration time.Duration.

---
+++ config.go
@@ -0,0 +1,36 @@
+package ddns
+
+import (
+	"time"
+)
+
+type Config struct {
+	PublicIPv4 string        `toml:"pub_ipv4"`
+	PublicIPv6 string        `toml:"pub_ipv6"`
+	Period     time.Duration `toml:"period"`
+	Timeout    time.Duration `toml:"timeout"`
+	ProxyURL   string        `toml:"proxy_url"`
+
+	Provider struct {
+		Dir  string   `toml:"dir"`
+		Item []string `toml:"item"`
+	} `toml:"provider"`
+}
+
+// duration is patch for toml v2.
+type duration time.Duration
+
+// MarshalText implement encoding.TextMarshaler.
+func (d *duration) MarshalText() ([]byte, error) {
+	return []byte(time.Duration(*d).String()), nil
+}
+
+// UnmarshalText implement encoding.TextUnmarshaler.
+func (d *duration) UnmarshalText(b []byte) error {
+	x, err := time.ParseDuration(string(b))
+	if err != nil {
+		return err
+	}
+	*d = duration(x)
+	return nil
+}
