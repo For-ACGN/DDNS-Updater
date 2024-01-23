93bd296371773a3470a76a20908da701bdce0ffe
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-01-23 18:08:13 +0800

    update logger_test.go, add TestLogger for test eatch methods.

--- logger_test.go
+++ logger_test.go
@@ -1,1 +1,31 @@
 package ddns
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestLogger(t *testing.T) {
+	l, err := newLogger("testdata/testdata.log")
+	require.NoError(t, err)
+
+	l.Info("info")
+	l.Infof("infof: %s", "info")
+
+	l.Warning("warning")
+	l.Warningf("warningf: %s", "warning")
+
+	l.Error("error")
+	l.Errorf("errorf: %s", "error")
+
+	l.Fatal("func", "fatal")
+	l.Fatalf("func", "fatalf: %s", "fatal")
+
+	err = l.Close()
+	require.NoError(t, err)
+
+	err = os.Remove("testdata/testdata.log")
+	require.NoError(t, err)
+}
