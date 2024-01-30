84d4b1d0dc443390db2d3134182f056e37ca408a
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-01-30 20:18:47 +0800

    add github.com/kardianos/service to warp program to a service.

--- cmd/main.go
+++ cmd/main.go
@@ -5,22 +5,36 @@ import (
 	"flag"
 	"log"
 	"os"
-	"os/signal"
 
+	"github.com/kardianos/service"
 	"github.com/pelletier/go-toml/v2"
 
 	"github.com/For-ACGN/DDNS-Updater"
 )
 
 var (
-	cfgPath string
-	upOnce  bool
+	cfgPath   string
+	upOnce    bool
+	install   bool
+	uninstall bool
 )
 
 func init() {
 	flag.StringVar(&cfgPath, "config", "config.toml", "configuration file path")
 	flag.BoolVar(&upOnce, "once", false, "update domain once")
+	flag.BoolVar(&install, "install", false, "install service")
+	flag.BoolVar(&uninstall, "uninstall", false, "uninstall service")
 	flag.Parse()
+}
+
+type config struct {
+	ddns.Config
+
+	Service struct {
+		Name        string `toml:"name"`
+		DisplayName string `toml:"display_name"`
+		Description string `toml:"description"`
+	} `toml:"service"`
 }
 
 func main() {
@@ -29,11 +43,11 @@ func main() {
 	decoder := toml.NewDecoder(bytes.NewReader(cfgData))
 	decoder.DisallowUnknownFields()
 
-	var config ddns.Config
-	err = decoder.Decode(&config)
+	var cfg config
+	err = decoder.Decode(&cfg)
 	checkError(err)
 
-	updater, err := ddns.NewUpdater(&config)
+	updater, err := ddns.NewUpdater(&cfg.Config)
 	checkError(err)
 
 	if upOnce {
@@ -41,19 +55,57 @@ func main() {
 		return
 	}
 
-	updater.Run()
-	updater.Update()
+	// initialize service
+	program := program{updater: updater}
+	svcConfig := service.Config{
+		Name:        cfg.Service.Name,
+		DisplayName: cfg.Service.DisplayName,
+		Description: cfg.Service.Description,
+	}
+	svc, err := service.New(&program, &svcConfig)
+	checkError(err)
 
-	// stop signal
-	signalCh := make(chan os.Signal, 1)
-	signal.Notify(signalCh, os.Interrupt)
-	<-signalCh
-
-	updater.Stop()
+	// switch operation
+	switch {
+	case install:
+		err = svc.Install()
+		if err != nil {
+			log.Fatalln("failed to install service:", err)
+		}
+		log.Println("install service successfully")
+	case uninstall:
+		err = svc.Uninstall()
+		if err != nil {
+			log.Fatalln("failed to uninstall service:", err)
+		}
+		log.Println("uninstall service successfully")
+	default:
+		lg, err := svc.Logger(nil)
+		checkError(err)
+		err = svc.Run()
+		if err != nil {
+			_ = lg.Error(err)
+		}
+	}
 }
 
 func checkError(err error) {
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
+}
+
+type program struct {
+	updater *ddns.Updater
+}
+
+func (p *program) Start(service.Service) error {
+	p.updater.Run()
+	p.updater.Update()
+	return nil
+}
+
+func (p *program) Stop(service.Service) error {
+	p.updater.Stop()
+	return nil
 }
